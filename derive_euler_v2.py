121c4bfb2672d7a50156ec1c6c893a9f5a2a4f7f
tiroumalaifreddy <36243250+tiroumalaifreddy@users.noreply.github.com>
2018-02-11 20:09:32 +0100

    Update derive_euler_v2.py

--- derive_euler_v2.py
+++ derive_euler_v2.py
@@ -4,8 +4,8 @@ def derivee_de_u_pr_x(u,dx):
     du=(u[1:]-u[:-1])/dx
     return du
     
-def u_instant_suivant(c,u,dx):
-    u_t_suivant=u-(c*derivee_de_u_pr_x(u,dx))
+def u_instant_suivant(c,u,dx,dt):
+    u_t_suivant=u-(c*derivee_de_u_pr_x(u,dx)*dt)
     return u_t_suivant
 
 print(u_instant_suivant(5,np.arange(1,9,1),0.1))
