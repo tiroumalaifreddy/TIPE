fa431215f0e16417f383df485972d7912cf900b9
tiroumalaifreddy <36243250+tiroumalaifreddy@users.noreply.github.com>
2018-02-18 20:52:17 +0100

    Update derive.py

--- derive.py
+++ derive.py
@@ -2,12 +2,9 @@ import numpy as np
 
 
 def u_suivant(u, c, dx, dt):
-
     def f(u):
         return 1-u
-
     u[0] = 0
-
     for i in range(1, len(u)):
         u[i] = u[i-1] - c*f(u[i-1])*dt
        
