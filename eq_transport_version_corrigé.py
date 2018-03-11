d7abfeb7642af82a04681275bb06b6e001eb497c
tiroumalaifreddy <36243250+tiroumalaifreddy@users.noreply.github.com>
2018-03-11 18:47:25 +0100

    Update eq_transport_version_corrigé.py

--- eq_transport_version_corrigé.py
+++ eq_transport_version_corrigé.py
@@ -4,25 +4,15 @@ import matplotlib.pyplot as plt
 
 
 def u_suivant(u, c, dx, dt, nb):
-
     l=[u]
-
     for t in range(nb):
-
         du = (u[1:]-u[:-1])/dx
-
         u[1:] -= c*du*dt
-
         if t > 500:
-
             u[0] = 0
-
         else:
-
             u[1] = 1
-
         l.append(u.copy())
-
     return l
 
 
@@ -32,11 +22,8 @@ l=u_suivant(u_initial, 1, 0.1, .01,1000)
 
 
 for i in range(len(l)):
-
     if i%100==0:
-
         plt.plot(l[i])
-
 
 
 plt.show()
